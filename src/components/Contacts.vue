f8cf88c27c82aeb7da27ff4ba89bbf567b7caf49
mbaSnap <you@example.com>
2018-12-09 20:23:13 +0300

    sds

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,5 +1,56 @@
 <template>
-    
+<div>
+<div class="col4-2 first tb-column">
+														
+<!-- module text -->
+<div id="text-594-1-0-0" class="module module-text text-594-1-0-0 requisites ">
+	
+		
+	<p><strong>ООО «КОМПИС»</strong><br>ул. Фонтанна, <em>59</em><br>г. Мариуполь,<br>Донецкая&nbsp;область,<br>Украина, 87515.<br>Код ОКПО 32794731, МФО 351005<br>ИНН 327947305696, Свидетельство НДС № 100306564<br>Р/с 26005350543200 в АТ "УкрСиббанк"</p><p>&nbsp;</p>
+	</div>
+<!-- /module text -->
+
+<!-- module divider -->
+<div id="divider-594-1-0-1" class="module module-divider divider-594-1-0-1 dotted " style="border-width: 1px;border-color: #bfbdbd;margin-top: -30px;">
+	</div>
+<!-- /module divider -->
+
+<!-- module text -->
+<div id="text-594-1-0-2" class="module module-text text-594-1-0-2 requisites ">
+	
+		
+	<p><strong>Директор</strong></p><p><strong>Ионов&nbsp;Андрей&nbsp;Владимирович</strong><br>Телефон приёмной: +380 (62) 384–92-90<br>E-mail: <a href="mailto:ionov@compis.com.ua" target="_blank">ionov@compis.com.ua</a></p>
+	</div>
+<!-- /module text -->
+</div>
+
+<div class="col-full first tb-column">
+									
+					
+<!-- module divider -->
+<div id="divider-594-2-0-0" class="module module-divider divider-594-2-0-0 dotted " style="border-width: 1px;border-color: #c4bebe;">
+</div>
+<!-- /module divider -->
+
+<!-- module text -->
+<div id="text-594-2-0-1" class="module module-text text-594-2-0-1  ">
+
+
+<p><strong>Коммерческий отдел продаж</strong><br> Телефон: +380 (95) 624–81-82<br> E-mail: &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<a href="mailto:trade@compis.com.ua">trade@compis.com.ua</a></p>
+</div>
+<!-- /module text -->
+
+<!-- module text -->
+<div id="text-594-2-0-2" class="module module-text text-594-2-0-2  ">
+
+
+<p><strong>Бухгалтерия</strong><br> Телефон: +380 (95) 624–81-82<br> E-mail: &nbsp; <a href="mailto:booker@compis.com.ua">booker@compis.com.ua</a></p>
+</div>
+<!-- /module text -->
+
+</div>
+
+</div>  
 </template>
 
 <script>
